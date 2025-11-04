511d83c680cc425d99c0fa6f328193b81f36269b
NicolasKnittel <ohsiixo@gmail.com>
2025-11-04 13:41:33 +0100

    Ajout front: layouts, components + librairies

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp class="flex flex-col min-h-screen">
+    <AppHeader />
+
+    <main class="flex-1">
+      <h1 class="text-lg">Ceci est un titre</h1>
+      <!-- main content here -->
+    </main>
+
+    <AppFooter />
+  </UApp>
 </template>
