c044edbd05fb9a1a52a5f318dde9a173a5c00c84
Nicolas Knittel <ohsiixo@gmail.com>
2025-11-09 00:19:43 +0100

    About done, graphs WIP

--- frontend/app/pages/(public)/about.vue
+++ frontend/app/pages/(public)/about.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Oui</h1>
+  <div
+    class="flex flex-col justify-center items-center p-8 max-w-4xl mx-auto text-center gap-4"
+  >
+    <h1>About Prime Trading</h1>
+    <p>
+      Prime Trading is a school project aimed at exploring all the facets of web
+      development, including planning, designing, front-end and back-end
+      technologies, as well as database management and deployment strategies.
+    </p>
+    <p>
+      The goal is to create a fully functional web application that showcases my
+      skills and knowledge in these areas.
+    </p>
+  </div>
 </template>
